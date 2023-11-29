7350f1c956ab70da4dfc8077322a6a6c6174d959
JER3MIAH <jeremiahbrownie@gmail.com>
2023-11-29 22:26:11 +0100

    Code clean up

--- lib/presentation/features/main_notes/views/bottom_nav_bar.dart
+++ lib/presentation/features/main_notes/views/bottom_nav_bar.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/common/utils/page_transition.dart';
 import 'package:notes_app/data/models/models.dart';
 import 'package:notes_app/logic/blocs/blocs.dart';
+import 'package:notes_app/presentation/features/main_notes/pages/add_new_note.dart';
+import 'package:notes_app/presentation/features/settings/settings_page.dart';
 
 class KbottomNavBar extends StatelessWidget {
   const KbottomNavBar({super.key});
@@ -53,19 +56,63 @@ class KbottomNavBar extends StatelessWidget {
                 children: [
                   IconButton(
                     onPressed: () {},
-                    icon: const Icon(Icons.delete),
+                    icon: const Icon(Icons.format_list_bulleted),
                   ),
                   const Spacer(),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.more_vert),
-                  ),
+                  PopupMenuButton(
+                    color: Theme.of(context).colorScheme.primary,
+                    // offset: Offset(0, 50),
+                    iconSize: 30,
+                    icon: const Icon(Icons.more_horiz),
+                    itemBuilder: (context) {
+                      return [
+                        popUpMenuButton(
+                            title: 'New note',
+                            icon: Icons.add,
+                            onTap: () {
+                              bottomToTopNavigation(
+                                  context, const AddNotePage());
+                            }),
+                        popUpMenuButton(
+                            title: 'New todo', icon: Icons.task, onTap: () {}),
+                        popUpMenuButton(
+                            title: 'Delete all notes',
+                            icon: Icons.delete_forever,
+                            onTap: () {}),
+                        popUpMenuButton(
+                            title: 'Settings',
+                            icon: Icons.settings,
+                            onTap: () {
+                              bottomToTopNavigation(
+                                context,
+                                const SettingPage(),
+                              );
+                            }),
+                      ];
+                    },
+                  )
                 ],
               ),
             ),
           );
         }
       },
+    );
+  }
+
+  PopupMenuItem<dynamic> popUpMenuButton({
+    required String title,
+    required IconData icon,
+    required void Function()? onTap,
+  }) {
+    return PopupMenuItem(
+      onTap: onTap,
+      child: ListTile(
+        titleTextStyle: const TextStyle(color: Colors.white),
+        iconColor: Colors.white,
+        leading: Icon(icon),
+        title: Text(title),
+      ),
     );
   }
 }
